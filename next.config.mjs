a61a4dc6420815c69513398af1262bd488095d63
heywesfrank <142755253+heywesfrank@users.noreply.github.com>
2026-02-19 16:54:45 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async headers() {
-    return [
-      {
-        // This targets ONLY your embeddable widget
-        source: "/widget",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: "frame-ancestors * https: http:;", 
-          }
-        ],
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
