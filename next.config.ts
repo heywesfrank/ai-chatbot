b06aa90cb7d7325925a1a28fa347d1d135d58df5
heywesfrank <142755253+heywesfrank@users.noreply.github.com>
2026-02-19 16:12:55 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,26 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        // This targets ONLY your embeddable widget
+        source: "/widget",
+        headers: [
+          {
+            // Modern browsers use Content-Security-Policy
+            key: "Content-Security-Policy",
+            value: "frame-ancestors *", 
+          },
+          {
+            // Older browsers rely on X-Frame-Options
+            key: "X-Frame-Options",
+            value: "ALLOWALL",
+          }
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
