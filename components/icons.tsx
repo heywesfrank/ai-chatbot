3e1437b35c3ea49460f90b3fd9a94cafa6d725d1
heywesfrank <142755253+heywesfrank@users.noreply.github.com>
2026-02-23 20:01:51 -0500

    Update icons.tsx

--- components/icons.tsx
+++ components/icons.tsx
@@ -1,3 +1,4 @@
+// components/icons.tsx
 import React from 'react';
 
 export const CopyIcon = ({ className }: { className?: string }) => (
@@ -89,4 +90,8 @@ export const LinkIcon = ({ className }: { className?: string }) => (
 
 export const UsersIcon = ({ className }: { className?: string }) => (
   <svg className={className} fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" /></svg>
+);
+
+export const CpuIcon = ({ className }: { className?: string }) => (
+  <svg className={className} fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 2 0 00-2-2H7a2 2 0 00-2 2v10a2 2 0 002 2zM9 9h6v6H9V9z" /></svg>
 );
