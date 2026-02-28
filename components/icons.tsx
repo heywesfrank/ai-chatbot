0c9a8f328fc353e2538e945177a0d2725284f7b0
heywesfrank <142755253+heywesfrank@users.noreply.github.com>
2026-02-28 13:40:55 -0500

    Update icons.tsx

--- components/icons.tsx
+++ components/icons.tsx
@@ -151,3 +151,9 @@ export const HomeIcon = ({ className }: { className?: string }) => (
     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
   </svg>
 );
+
+export const GripVerticalIcon = ({ className }: { className?: string }) => (
+  <svg className={className} fill="none" stroke="currentColor" viewBox="0 0 24 24" strokeWidth={2}>
+    <path strokeLinecap="round" strokeLinejoin="round" d="M8 9h2v2H8V9zm0 4h2v2H8v-2zm0 4h2v2H8v-2zm6-8h2v2h-2V9zm0 4h2v2h-2v-2zm0 4h2v2h-2v-2z" />
+  </svg>
+);
