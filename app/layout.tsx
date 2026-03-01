419d060c2d9e15b2995726604d83feb9c3585e77
heywesfrank <142755253+heywesfrank@users.noreply.github.com>
2026-02-28 20:18:17 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,11 @@ import './globals.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'Knowledge Base Bot',
-  description: 'Minimalist RAG Chatbot',
+  title: {
+    template: '%s | Apoyo',
+    default: 'Apoyo',
+  },
+  description: 'AI Customer Support Suite',
 };
 
 export default function RootLayout({
